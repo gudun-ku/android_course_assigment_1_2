34866ca0495ac2d7054d2c5474d1ba109f224872
Aleksandr Beloushkin <aleks.beloushkin@yahoo.com>
2018-12-29 13:10:52 +0300

    Fragment helper added

--- app/src/main/java/com/beloushkin/android/learn/assigment_1_2/utils/FragmentChangeHelper.java
+++
@@ -1,4 +0,0 @@
-package com.beloushkin.android.learn.assigment_1_2.utils;
-
-public class FragmentChangeHelper {
-}
---
+++ app/src/main/java/com/beloushkin/android/learn/assigment_1_2/utils/FragmentsHelper.java
@@ -0,0 +1,28 @@
+package com.beloushkin.android.learn.assigment_1_2.utils;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.view.ViewGroup;
+
+public class FragmentsHelper {
+
+    public static void replaceFragment(FragmentActivity fragmentActivity
+            , Fragment fragment, int containerId) {
+        fragmentActivity.getSupportFragmentManager()
+                .beginTransaction()
+                .replace(containerId, fragment)
+                .commit();
+
+    }
+
+    public static void replaceFragmentWithBack(FragmentActivity fragmentActivity
+            , Fragment fragment, int containerId) {
+        fragmentActivity.getSupportFragmentManager()
+                .beginTransaction()
+                .replace(containerId, fragment)
+                .addToBackStack(null)
+                .commit();
+
+    }
+
+}
